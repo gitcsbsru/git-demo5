2c8425cf4833c17a3d765438471eeefd27d246cc
WindzTrumpet <chinapat.dev@gmail.com>
2019-09-09 22:27:54 +0700

    Initial project with git demo template

--- git-demo.sh
+++ git-demo.sh
@@ -5,7 +5,7 @@ echo "Add source code"
 git add .
 echo "Commit source code"
 git commit -m "Initial project with git demo template"
-for i in 1 2;
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20;
 do
   echo "Add remote to github origin"
   git remote add origin https://github.com/gitcsbsru/git-demo$i.git
